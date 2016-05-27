f59916fb88595f80b93d25627e05dac37f84aea0
syntaxsizer <kakuma4never@gmail.com>
2016-05-27 09:50:46 +0300

    Initial commit

---
+++ app/src/main/java/com/android/qusai/golom/first.java
@@ -0,0 +1,14 @@
+package com.android.qusai.golom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class first extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+}
