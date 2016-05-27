3e07c92cf5d40691e7d674abb37fb416cf3c6e30
syntaxsizer <kakuma4never@gmail.com>
2016-05-27 17:30:25 +0300

    add listview

--- app/src/main/java/com/android/qusai/golom/Profile.java
+++ app/src/main/java/com/android/qusai/golom/Profile.java
@@ -1,7 +1,18 @@
 package com.android.qusai.golom;
 
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by qusai on 5/27/16.
  */
-public class Profile {
+public class Profile extends ListActivity{
+
+String [] menuList ={"Home", "Calender","Overview","Groups", "Lists","Profile","Timeline","   Settings","   Logout"};
+protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menuList));
+}
+
 }
--- app/src/main/java/com/android/qusai/golom/first.java
+++ app/src/main/java/com/android/qusai/golom/first.java
@@ -1,7 +1,9 @@
 package com.android.qusai.golom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class first extends AppCompatActivity {
 
@@ -9,6 +11,10 @@ public class first extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+    public void showlist(View v){
+
+        Intent intent=new Intent(this,Profile.class);
     }
 
 }
