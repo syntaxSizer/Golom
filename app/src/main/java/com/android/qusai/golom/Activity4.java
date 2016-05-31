cb1405323b3f90973e6fdc99fb48e28fe92db9a6
syntaxsizer <kakuma4never@gmail.com>
2016-05-31 07:40:36 +0300

    link home btn to activity 4

--- app/src/main/java/com/android/qusai/golom/Activity4.java
+++ app/src/main/java/com/android/qusai/golom/Activity4.java
@@ -9,11 +9,11 @@ import android.widget.ArrayAdapter;
  * Created by qusai on 5/28/16.
  */
 public class Activity4 extends ListActivity {
-
-    String [] menuList ={"Home", "Calender","Overview","Groups", "Lists","Profile","Timeline","Settings","Logout"};
-protected void onCreate(Bundle icicle){
-    super.onCreate(icicle);
-    ArrayAdapter arrayadapter = new ArrayAdapter<String>(this,R.layout.profile, menuList);
-    setListAdapter(arrayadapter);
-}
+//
+//    String [] menuList ={"Home", "Calender","Overview","Groups", "Lists","Profile","Timeline","Settings","Logout"};
+//protected void onCreate(Bundle icicle){
+//    super.onCreate(icicle);
+//    ArrayAdapter arrayadapter = new ArrayAdapter<String>(this,R.layout.profile, menuList);
+//    setListAdapter(arrayadapter);
+//}
 }
--- app/src/main/java/com/android/qusai/golom/Profile.java
+++ app/src/main/java/com/android/qusai/golom/Profile.java
@@ -2,9 +2,13 @@ package com.android.qusai.golom;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by qusai on 5/27/16.
@@ -15,6 +19,25 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        btntwo();
+    }
+
+    public void btntwo(){
+        TextView button =(TextView) findViewById(R.id.textView4);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Profile.this,Activity4.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
+
+
+
 
